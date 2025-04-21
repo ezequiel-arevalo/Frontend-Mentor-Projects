e0b20709f947fd32fcf017fe2b528c738bc4aa8c
Ezequiel Arevalo <ezequiel-arevalo@outlook.com>
2025-04-21 19:46:28 -0300

    feat: finish challenge!

--- interactive-rating/src/js/app.js
+++ interactive-rating/src/js/app.js
@@ -1,1 +1,22 @@
 'use strict';
+
+let numberIndex = null;
+let numberActive = document.querySelectorAll('.rating-card__numbers-item');
+numberActive.forEach((selected) => {
+  selected.addEventListener("click", (e) => {
+    numberIndex = selected.textContent.trim();
+    console.log(numberIndex);
+  });
+});
+
+let contentReview = document.querySelector('.rating-card');
+let contentSolution = document.querySelector('.thank-you-card');
+let userReview = document.getElementById('userReview');
+let form = document.querySelector('.rating-card__content');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  form.style.display = "none";
+  contentSolution.style.display = "block";
+  userReview.textContent = numberIndex;
+});
